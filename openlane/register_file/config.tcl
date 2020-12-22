fe888d4a2293f65da76794b9fe4cd04ab42170a0
Anton Blanchard <anton@linux.ibm.com>
2020-12-23 07:18:21 +1100

    No need to set FP_CORE_UTIL when FP_SIZING is set to absolute

--- openlane/register_file/config.tcl
+++ openlane/register_file/config.tcl
@@ -12,14 +12,16 @@ set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 1200 1200"
 
+# Settings for macros
 set ::env(DESIGN_IS_CORE) 0
 set ::env(FP_PDN_CORE_RING) 0
 set ::env(GLB_RT_MAXLAYER) 5
+
+# Tuning
 set ::env(GLB_RT_ADJUSTMENT) 0.1
 set ::env(SYNTH_STRATEGY) 2
 set ::env(SYNTH_MAX_FANOUT) 6
-set ::env(FP_CORE_UTIL) 50
-set ::env(PL_TARGET_DENSITY) [ expr ($::env(FP_CORE_UTIL)+5) / 100.0 ]
+set ::env(PL_TARGET_DENSITY) 0.55
 set ::env(CELL_PAD) 5
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
